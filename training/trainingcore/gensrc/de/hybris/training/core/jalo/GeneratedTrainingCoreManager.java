09a28436d9d47e162e03d43c390688bb3d137cc4
mihail <mihaila4038@gmail.com>
2018-08-06 09:33:43 +0300

    no message

--- training/trainingcore/gensrc/de/hybris/training/core/jalo/GeneratedTrainingCoreManager.java
+++ training/trainingcore/gensrc/de/hybris/training/core/jalo/GeneratedTrainingCoreManager.java
@@ -1,7 +1,7 @@
 /*
  * ----------------------------------------------------------------
  * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
- * --- Generated at Aug 2, 2018 5:25:32 PM                      ---
+ * --- Generated at Aug 6, 2018 9:22:31 AM                      ---
  * ----------------------------------------------------------------
  *  
  * [y] hybris Platform
